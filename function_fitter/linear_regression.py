28a025a5a5c16e97c33f5025a5f646ae89a015f1
Christian Saravia <csaravia@Christian.local>
2018-03-07 10:56:32 +0900

    Fixed bug

--- function_fitter/linear_regression.py
+++ function_fitter/linear_regression.py
@@ -1,35 +1,35 @@
 import tensorflow as tf
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 class LinearRegression(object):
     """docstring for LinearRegression."""
     def __init__(self):
         super(LinearRegression, self).__init__()
+        self.weights = None
+        self.bias = None
 
     def random_mini_batches(self, X, Y, batch_size = 64):
         """Pending"""
-        return X, Y
+        return [X], [Y]
 
-    def initialize_parameter(self, X):
-        m = X.shape[0]
-        nx = np.ones((m,3))
-        for j in range(m):
-            for i in range(2):
-                nx[j][i + 1] = nx[j][i] * X[j]
-        return nx
+    def fit(self, X, Y, epochs = 1000, batch_size = 64, lr = 0.0001, l2_reg = 0, print_rate = 50):
+        tf.reset_default_graph()
 
-    def fit(self, X, Y, epochs = 1000, batch_size = 64, lr = 0.0001):
-        X = self.initialize_parameter(X)
         m, n = X.shape
 
-        x = tf.placeholder(tf.float32, name='x')
-        y = tf.placeholder(tf.float32, name='y')
-        W = tf.Variable(np.random.randn(m, n), dtype = 'float32', name='W')
+        x = tf.placeholder(tf.float32, shape=(None, n), name='x')
+        y = tf.placeholder(tf.float32, shape=(None, 1), name='y')
+        W = tf.Variable(np.random.randn(n, 1), dtype = 'float32', name='W')
         b = tf.Variable(np.random.randn(n), dtype = 'float32', name='b')
-        z = tf.add(tf.multiply(x,W), b)
+        z = tf.add(tf.matmul(x,W), b)
+
+        # Regularization
+        regularizers = tf.nn.l2_loss(W)
 
         # Mean square error
-        cost = tf.reduce_sum(tf.pow(z - y, 2)) / (2 * m)
+        cost = tf.reduce_sum(tf.pow(z - y, 2) / (2 * m) + l2_reg * regularizers)
 
         #Gradient descent
         gd = tf.train.GradientDescentOptimizer(learning_rate = lr, name = 'gd')
@@ -45,17 +45,25 @@ class LinearRegression(object):
                 for (x_train, y_train) in zip(x_shuffle, y_shuffle):
                     sess.run(optimizer, feed_dict={'x:0':x_train, 'y:0':y_train})
 
-                if epoch % 50 == 0:
-                    current_cost = sess.run(cost, feed_dict={'x:0':x_shuffle, 'y:0':y_shuffle})
-                    print('epoch: %3d  cost: %11.9lf b: %11.9lf'%(epoch, current_cost, sess.run(b)))
-                    print('W: ', sess.run(W))
-                    # print('epoch: %3d  cost: %11.9lf  W: %11.9lf  b: %11.9lf'%(epoch, current_cost, sess.run(W), sess.run(b)))
+                if epoch % print_rate == 0:
+                    current_cost = sess.run(cost, feed_dict={'x:0':X, 'y:0':Y})
+                    print('epoch: %3d  cost: %11.9lf'%(epoch, current_cost))
+                    # print('W: ', sess.run(W))
+                    # print('b: ', sess.run(b))
+                    # print('-' * 5)
 
-
+            self.weights = sess.run(W)
+            self.bias = sess.run(b)
             training_cost = sess.run(cost, feed_dict={'x:0':X, 'y:0':Y})
             print('Training cost: ', training_cost)
 
 
+    def predict(self, X):
+        W, b = self.weights, self.bias
+        return np.dot(X, W) + b
+
+    def get_weights_bias(self):
+        return np.array(self.weights), self.bias
 
 
 
@@ -65,19 +73,40 @@ if __name__ == '__main__':
     b = 6
     c = 2
 
-    X = []
-    Y = []
     m = 20
-    for i in range(m):
+    n = 3
+
+    epochs = 15000
+    lr = 0.1
+    print_rate=5000
+    l2_reg = 0
+
+    X = np.ones((m, n))
+    Y = np.ones((m, 1))
+    for j in range(m):
         x = np.random.rand()
         y = a * x * x + b * x + c
-        X.append(x)
-        Y.append(y)
+        for i in range(n - 1):
+            X[j][i + 1] = X[j][i] * x
+        Y[j][0] = y
 
-    X = np.array(X)
-    Y = np.array(Y)
-    # X = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-    # Y = np.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221, 2.827,3.465,1.65,2.904,2.42,2.94,1.3])
     linreg = LinearRegression()
-    linreg.fit(X, Y, epochs = 1000, lr = 0.01)
-    print('FINISH')
+    print('epochs: %d learning_rate: %.04lf'%(epochs, lr))
+    print('-' * 30)
+
+    print('Training with regularizacion: ',l2_reg)
+    linreg.fit(X, Y, epochs=epochs, lr =lr, print_rate=print_rate)
+    coef, bias = linreg.get_weights_bias()
+    print('Expected coefficients: A: %.04lf  B: %.04lf  C: %.04lf'%(a, b, c))
+    print('Predicted coefficients: A: %.04lf  B: %.04lf  C: %.04lf'%(coef[2], coef[1], coef[0]))
+    print('Bias :  A: %.04lf  B: %.04lf  C: %.04lf'%(bias[2], bias[1], bias[0]))
+    print('-' * 30)
+
+    l2_reg = 0.001
+    print('Training with regularizacion: ',l2_reg)
+    linreg.fit(X, Y, epochs=epochs, lr=lr, l2_reg=l2_reg, print_rate =print_rate)
+    coef, bias = linreg.get_weights_bias()
+    print('Expected coefficients: A: %.04lf  B: %.04lf  C: %.04lf'%(a, b, c))
+    print('Predicted coefficients: A: %.04lf  B: %.04lf  C: %.04lf'%(coef[2], coef[1], coef[0]))
+    print('Bias :  A: %.04lf  B: %.04lf  C: %.04lf'%(bias[2], bias[1], bias[0]))
+    print('-' * 30)
